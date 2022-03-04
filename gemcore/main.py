b26bee3a88ccaed33cd10edc3bb0de40a4a7490c
Natalia <nataliabidart@gmail.com>
2022-03-03 22:45:36 -0300

    Fix style errors, add badges to README

--- gemcore/main.py
+++ gemcore/main.py
@@ -33,5 +33,4 @@ def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
 @app.get("/items/", response_model=List[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
-    from pprint import pprint; pprint(items)
     return items
